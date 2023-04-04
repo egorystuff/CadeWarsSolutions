6362866eb989905e2ddac5bcf019c89e4a0141e6
Yahor Yankavets <mr.yankovec@gmail.com>
2023-04-04 18:07:45 +0300

    Returning Strings

---
+++ Returning Strings.js
@@ -0,0 +1,12 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name) {
+  let str = `Hello, ${name} how are you doing today?`;
+  return str;
+  //your code here
+}
+
+let value = greet('Ryan');
+console.log(value);
