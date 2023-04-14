479f926fb4e86e673301318c819809054752053a
Yahor Yankavets <mr.yankovec@gmail.com>
2023-04-14 15:08:31 +0300

    Create Sum of Numbers.js

---
+++ 7_kyu/Sum of Numbers.js
@@ -0,0 +1,34 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+function getSum(a, b) {
+  if (a === b) return a;
+  let sum = 0;
+
+  if (a <= b) {
+    while (a <= b) {
+      sum += a;
+      a++;
+    }
+  } else if (b <= a) {
+    while (b <= a) {
+      sum += b;
+      b++;
+    }
+  }
+
+  return sum;
+}
+
+let value = getSum(0, 1);
+console.log(value);
