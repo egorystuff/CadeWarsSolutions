386df98e48f5a0ca3eafde65c36c6438cc92f546
egorystuff <mr.yankovec@gmail.com>
2023-04-19 22:56:10 +0300

    Create Beginner Series #2 Clock.js

---
+++ 8_kyu/Beginner Series #2 Clock.js
@@ -0,0 +1,18 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+
+function past(h, m, s) {
+  return (h * 3600 + m * 60 + s) * 1000;
+}
+
+let strictEqual = past(1, 0, 1);
+
+console.log(strictEqual);
