8b891c047d68840f1fec407c6dd4d538573bcad2
egorystuff <mr.yankovec@gmail.com>
2023-05-04 23:04:26 +0300

    Create Sum The Strings.js

---
+++ 8_kyu/Sum The Strings.js
@@ -0,0 +1,9 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+let sumStr = (a, b) => String(Number(a) + Number(b));
+
+let value = sumStr('4', '5');
+
+console.log(value);
