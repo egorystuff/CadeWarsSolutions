eae9f0eab78e377d62f42475a608c84ef8499f9f
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-16 14:37:58 +0300

    Create Sort and Star.js

---
+++ 8_kyu/Sort and Star.js
@@ -0,0 +1,27 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+function twoSort(s) {
+	let arr = s.sort();
+	let res = arr[0];
+	return res.split('').join('***');
+}
+
+let value = twoSort([
+	'turns',
+	'out',
+	'random',
+	'test',
+	'cases',
+	'are',
+	'easier',
+	'than',
+	'writing',
+	'out',
+	'basic',
+	'ones',
+]);
+console.log(value);
