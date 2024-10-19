4c63a74ed85f732e213ac9b987a70aba339c60b7
Yahor Yankavets <mr.yankovec@gmail.com>
2024-10-19 23:26:16 +0300

    new kata and update

--- JavaScript/6_kyu/Who likes it.js
+++ JavaScript/6_kyu/Who likes it.js
@@ -13,10 +13,23 @@ function likes(names) {
   if (names.length === 0) return `no one likes this`;
   if (names.length === 1) return `${names[0]} likes this`;
   if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
-  if (names.length === 3)
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  if (names.length > 3)
-    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if (names.length > 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+}
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
 }
 
 let value = likes(["Alex", "Jacob", "Mark"]);
