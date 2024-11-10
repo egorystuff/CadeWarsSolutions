9d41380da5007c4c3a9513df5b96cb0c33b59ed8
Yahor Yankavets <mr.yankovec@gmail.com>
2024-11-10 23:43:04 +0300

    Update Factorial.js

--- JavaScript/7_kyu/Factorial.js
+++ JavaScript/7_kyu/Factorial.js
@@ -1,5 +1,13 @@
-// Your task is to write function factorial.
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
 
-// https://en.wikipedia.org/wiki/Factorial
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
 
-const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1));
+// More details about factorial can be found here.
+
+function factorial(n) {
+  if (n < 0 && n > 12) throw new RangeError("Input must be between 0 and 12");
+  if (n === 0) return 1;
+  return n < 2 ? 1 : n * factorial(n - 1);
+}
+
+console.log(factorial(13));
